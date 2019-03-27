a1344164fd8fba0d0102322f9a489b251b5cfdb7
ahmedbali19 <48994905+ahmedbali19@users.noreply.github.com>
2019-03-27 16:18:08 +0100

    Add files via upload

---
+++ Projet Web/js/checkout.js
@@ -0,0 +1,10 @@
+function test(){
+
+var mail = f.mail.value;
+var numtel = f.numtel.value
+
+if( mail.length== 0 || numtel.length== 0)
+{
+alert("Vérifier votre mail et votre numero de telephone");
+}
+}
